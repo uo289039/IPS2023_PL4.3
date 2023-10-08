84124b6c419821fe381dc1525c2005893b6122ba
uo289039 <uo289039@uniovi.es>
2023-10-08 20:09:15 +0200

    añadida info a la base de datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,17 +8,19 @@ delete from Atleta;
 delete from Categoria;
 delete from Competicion;
 
-insert into Competicion(id,inicio,fin,fecha,descr,cuota,distancia, id_cat) values 
-	(100,'2016-10-05','2016-10-25','2016-11-09','finalizada',50.0,5,1),
-	(101,'2016-10-05','2016-10-25','2016-11-10','en fase 3',50.5,5,2),
-	(102,'2016-11-05','2016-11-09','2016-11-20','en fase 2',50.0,5,3),
-	(103,'2016-11-10','2016-11-15','2016-11-21','en fase 1',50.0,5,5),
-	(104,'2016-11-11','2016-11-15','2016-11-22','antes inscripcion',50.0,5,4);
+insert into Competicion(id,inicio,fin,fecha,descr,cuota,distancia, id_cat,nPlazas) values 
+	(100,'2016-10-05','2016-10-25','2016-11-09','finalizada',50.0,5,1,40),
+	(101,'2016-10-05','2016-10-25','2016-11-10','en fase 3',50.5,5,2,30),
+	(102,'2016-11-05','2016-11-09','2016-11-20','en fase 2',50.0,5,3,50),
+	(103,'2016-11-10','2016-11-15','2016-11-21','en fase 1',50.0,5,5,33),
+	(104,'2016-11-11','2016-11-15','2016-11-22','antes inscripcion',50.0,5,4,42);
 
-insert into Atleta(dni,f_nacimiento,nombre,sexo,inscripcion ) values
-(1,'2004-11-03','Alexander','hombre','2016-10-10'),(3,'2004-1-04','Marta','mujer','2016-10-9'),
-(7,'2002-12-09','Hunter','hombre','2016-11-01'),
-(2,'2003-01-12','Emilie','mujer','2016-10-10'),(9,'2000-10-11','Alexa','mujer','2016-10-12');
+insert into Atleta(dni,f_nacimiento,nombre,sexo,inscripcion,formaPago,correoE) values
+(1,'2004-11-03','Alexander','hombre','2016-10-10','transferencia','alex@gmail.com'),
+(3,'2004-1-04','Marta','mujer','2016-10-9','tarjeta','marta@gmail.com'),
+(7,'2002-12-09','Hunter','hombre','2016-11-01','transferencia','hunter@gmail.com'),
+(2,'2003-01-12','Emilie','mujer','2016-10-10','tarjeta','emilie@gmail.com'),
+(9,'2000-10-11','Alexa','mujer','2016-10-12','tarjeta','alexa@gmail.com');
 
 insert into Participa(dni_at,id_c) values 
 (1,100),(2,103),(9,100),(7,102),(3,101);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -19,16 +19,18 @@ check(tipo in('montaña','ruta')));
 
 drop table if exists Competicion;
 create table Competicion(id int primary key not null,inicio date not null, fin date not null, fecha date not null, descr varchar(32), 
-cuota decimal(4,2), distancia decimal(4,2), id_cat int not null,
+cuota decimal(4,2), distancia decimal(4,2), id_cat int not null, nPlazas int not null,
 constraint FK_Competicion_Categoria Foreign Key (id_cat) references "Categoria" (id_categoria), 
 check(inicio<=fin),check(fin<fecha));
 
 drop table if exists Atleta;
 create table Atleta(dni int primary key not null,f_nacimiento date not null, nombre varchar(20) not null,
-sexo varchar(7) not null,inscripcion date not null,
-check(sexo in ('hombre','mujer')));
+sexo varchar(7) not null,inscripcion date not null, formaPago varchar(15) not null, correoE varchar(15) not null,
+check(sexo in ('hombre','mujer')), check(formaPago in ('transferencia','tarjeta')));
 
 drop table if exists Participa;
 create table Participa(dni_at int not null, id_c int not null, constraint pk_Participa PRIMARY KEY(dni_at,id_c), 
                         constraint FK_Participa_Competicion Foreign Key (id_c) references "Competicion" (id),
                         constraint FK_Participa_Atleta Foreign Key (dni_at) references "Competicion" (dni));
+
+                       
