145eca92ce5f0a7fa6300bdcd12ae0f6db12ff3c
Daniel <Daniel@DESKTOP-QACOVCB.home>
2023-10-09 10:04:09 +0200

    Trigger para calcular categoría

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -29,8 +29,59 @@ sexo varchar(7) not null,inscripcion date not null, formaPago varchar(15) not nu
 check(sexo in ('hombre','mujer')), check(formaPago in ('transferencia','tarjeta')));
 
 drop table if exists Participa;
-create table Participa(dni_at int not null, id_c int not null, constraint pk_Participa PRIMARY KEY(dni_at,id_c), 
+create table Participa(dni_at int not null, id_c int not null, categoria varchar(20), constraint pk_Participa PRIMARY KEY(dni_at,id_c), 
                         constraint FK_Participa_Competicion Foreign Key (id_c) references "Competicion" (id),
                         constraint FK_Participa_Atleta Foreign Key (dni_at) references "Competicion" (dni));
+
+CREATE TRIGGER calcular_categoria
+AFTER INSERT ON Atleta
+FOR EACH ROW
+BEGIN
+    DECLARE categoria varchar(20);
+    DECLARE edad int;
+    SET edad = DATEDIFF(YEAR, @f_nacimiento, GETDATE());
+    IF NEW.sexo = 'hombre' THEN
+        IF edad < 12 THEN
+            SET categoria = 'Masculino sub-12';
+        ELSEIF edad < 16 THEN
+            SET categoria = 'Masculino sub-16';
+        ELSEIF edad < 18 THEN
+            SET categoria = 'Masculino sub-18';
+        ELSEIF edad < 23 THEN
+            SET categoria = 'Masculino sub-23';
+        ELSEIF edad < 30 THEN
+            SET categoria = 'Masculino senior-30';
+        ELSEIF edad < 40 THEN
+            SET categoria = 'Masculino senior-40';
+        ELSEIF edad < 50 THEN
+            SET categoria = 'Masculino senior-50';
+        ELSEIF edad < 60 THEN
+            SET categoria = 'Masculino senior-60';
+        ELSEIF edad >= 60 THEN
+            SET categoria = 'Masculino veteranos';
+        END IF;
+    ELSEIF NEW.sexo = 'mujer' THEN
+        IF edad < 12 THEN
+            SET categoria = 'Femenino sub-12';
+        ELSEIF edad < 16 THEN
+            SET categoria = 'Femenino sub-16';
+        ELSEIF edad < 18 THEN
+            SET categoria = 'Femenino sub-18';
+        ELSEIF edad < 23 THEN
+            SET categoria = 'Femenino sub-23';
+        ELSEIF edad < 30 THEN
+            SET categoria = 'Femenino senior-30';
+        ELSEIF edad < 40 THEN
+            SET categoria = 'Femenino senior-40';
+        ELSEIF edad < 50 THEN
+            SET categoria = 'Femenino senior-50';
+        ELSEIF edad < 60 THEN
+            SET categoria = 'Femenino senior-60';
+        ELSEIF edad >= 60 THEN
+            SET categoria = 'Femenino veteranas';
+        END IF;
+    END IF;
+    INSERT INTO Participa (dni_at, id_c, categoria) VALUES (NEW.dni, null, categoria);
+END;
 
                        
