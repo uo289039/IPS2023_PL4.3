ea59624b7312d0e4f28d1e3ce042528f2a2eff55
UO287616 <UO287616@EII-L06-10.aulasuo.uniovi.es>
2023-10-31 13:48:54 +0100

    Añadido el FileUtil

---
+++ src/main/java/giis/demo/util/ui/FileUtil.java
@@ -0,0 +1,67 @@
+package giis.demo.util.ui;
+
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A utility class to read/write text lines from/to a text file
+ */
+public class FileUtil {
+
+	public List<String> readLines(String inFileName) throws FileNotFoundException {
+		
+		// Falta validar parámetros.
+		
+		List<String> res = new LinkedList<>();
+		
+		String line;
+		BufferedReader in = new BufferedReader(new FileReader(inFileName));
+		
+		try {
+			try {
+				while((line = in.readLine()) != null) {
+					res.add(line);
+				}
+			} finally {
+				in.close();
+			}
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		
+		return res;
+		
+	}
+
+	public void writeLines(String outFileName, List<String> lines) {
+		
+		// Falta validar parámetros.
+		
+		try {
+			BufferedWriter out = new BufferedWriter(new FileWriter(outFileName));
+			try {	
+				int lineNumber = 0;
+				for(String line: lines) {
+					lineNumber++;
+					out.write(line);
+					if(lineNumber < lines.size()-1) {
+						out.newLine();
+					}
+				} 	
+			} finally {	
+				out.close();
+			}
+		} catch(IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+}
